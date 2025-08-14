0911ee3fda640fd6f7cb45d9931f06b4cbdc5433
Shahrukh-SK1 <sharukhayub6111@gmail.com>
2025-08-14 19:27:10 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,29 @@
+
+
+
+// Answer No .2
+
+var num1 = +prompt("Enter the first integar");
+var num2 = +prompt("Enter the second integar");
+
+if (num1 > num2) {
+    alert(num1 + " is larger");
+} else if (num2 > num1) {
+    alert(num2 + " is larger");
+} else {
+    alert("Both numbers are equal");
+}
+
+
+
+// Answer No .3
+
+var num = +prompt("Enter the number");
+
+if (num > 0){
+    alert("Number is Positve");
+} else if (num < 0){
+    alert("Number is Negative");
+} else {
+    alert("Number is Zero");
+}
